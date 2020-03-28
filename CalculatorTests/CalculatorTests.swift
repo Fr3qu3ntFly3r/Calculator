0ce3e296c9e76ed91307efcd2f6d22b56ddc4fc9
Balazs Szamody <bszamody@gmail.com>
2020-03-28 18:36:25 +1100

    Github actions added

---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,25 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Balázs Szamódy on 28/3/20.
+//  Copyright © 2020 Szamódy Zs. Balázs. All rights reserved.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+
+}
